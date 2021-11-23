cd30b3efd71cc85c0e7c01fdfb5e2b457de03c15
maddrake220 <jwchoi4118@gmail.com>
2021-11-23 17:30:43 +0900

    feat: Add 3 list
    To test railing comma

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,15 @@
+fruits = ['Apple', 'Banana', 'PineApple']
+
+animals = [
+        'Lion',
+        'Tiger',
+        'Rabbit'
+        ]
+Clubs = [
+        'ManU',
+        'ManC'
+        ]
+
 for i in range(1,16+1):
     if i % 15 ==0:
     print('fizzbuzz')
